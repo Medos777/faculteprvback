899978342049275b0c4878b4e911188e43464920
aminegh50 <medaminegh50@gmail.com>
2023-08-31 12:10:42 +0100

    vf

--- model/Classe.js
+++ model/Classe.js
@@ -3,11 +3,16 @@ const Schema = mongoose.Schema;
 const Emploi = require('./Emploi')
 
 const AutoIncrement = require('mongoose-sequence')(mongoose);
-
-const Etudiant= require('./etudiant')
+const Etudiant = require('./etudiant');
 const ClassSchema = new Schema({
     name: { type: String, required: true },
     etudiants: [{ type: Schema.Types.ObjectId, ref: 'Etudiant' }],
-    emplois: { type: Schema.Types.ObjectId, ref: 'Emploi',required:false },
+    emplois: { type: Schema.Types.ObjectId, ref: 'Emploi', required: false },
+    lengthEtudiant: { type: Number }
+});
+
+ClassSchema.pre('save', function(next) {
+    this.lengthEtudiant = this.etudiants.length;
+    next();
 });
 module.exports = mongoose.model('Classe', ClassSchema);
